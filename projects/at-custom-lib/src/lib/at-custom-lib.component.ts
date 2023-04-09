9a94d2ddb6010d1acfe0db17faabdec506ebf401
shubhamsahookiwi <shubham.sahoo@kiwitech.com>
2023-04-09 17:17:59 +0530

    class added

--- projects/at-custom-lib/src/lib/at-custom-lib.component.ts
+++ projects/at-custom-lib/src/lib/at-custom-lib.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
       (click)="btnClick($event)"
       [style.backgroundColor]="bgColor"
       [style.color]="btnColor"
+      [class]="btnClass"
     >
       {{ btnLabel }}
     </button>
@@ -19,6 +20,7 @@ export class AtCustomLibComponent {
   @Input() btnLabel: any;
   @Input() btnColor: any;
   @Output() btnClicked: EventEmitter<any> = new EventEmitter<any>();
+  @Input() btnClass: any;
 
   btnClick(e: any) {
     this.btnClicked.emit(e);
